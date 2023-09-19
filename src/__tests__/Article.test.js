6e6ad30a192f0b25d610868f073294e26af5ea53
Melvin James <jmelvin1134@gmail.com>
2023-09-19 04:12:45 -0400

    test

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -9,3 +9,12 @@ test("displays the text 'please pass this test'", () => {
 
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
+// src/__tests__/Article.test.js
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
+
+  // add this line
+  screen.debug();
+
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+});
